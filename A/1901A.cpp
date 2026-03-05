e2c40ed01ab6587bf00f5bee4bb6ee728faa1b83
Shahadat Hossain <sht.kcn@gmail.com>
2026-03-06 01:44:53 +0600

    1901A touched

---
+++ A/1901A.cpp
@@ -0,0 +1,38 @@
+// A. Line Trip
+//link: https://codeforces.com/contest/1901/problem/A
+//username: iam-phoenix
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vs = vector<string>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+
+void solve() {
+    
+    ll n;
+    cin >> n;
+    
+    // Main logic goes here
+    
+}
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+       solve();
+    }
+
+    return 0;
+}
