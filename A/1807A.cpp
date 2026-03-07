e19095342b87e3066ada63764b58037d2f3c13d3
Shahadat Hossain <sht.kcn@gmail.com>
2026-03-08 01:39:06 +0600

    1807A solved

---
+++ A/1807A.cpp
@@ -0,0 +1,38 @@
+// A. Plus or Minus
+//link: https://codeforces.com/contest/1807/problem/A
+//username: iam-phoenix
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vs = vector<string>;
+using vll = vector<ll>;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+
+void solve() {
+    
+    int n, m, res;
+    cin >> n >> m >> res;
+    if(n-m == res) {cout << "-" << endl;}
+    
+    else{ cout << "+" << endl;}
+    
+}
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+       solve();
+    }
+    return 0;
+}
