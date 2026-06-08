b16ab17488261048db4a140cb795884499bf590d
Shahadat Hossain <sht.kcn@gmail.com>
2026-06-08 17:51:47 +0600

    200B solved

---
+++ B/200B.cpp
@@ -0,0 +1,45 @@
+// 
+//link: 
+//author: https://codeforces.com/profile/iam-phoenix
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vs = vector<string>;
+using vl = vector<ll>;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define allr(a) a.rbegin(), a.rend()
+#define sz(x) (int)(x).size()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define endl '\n'
+
+
+void solve() {
+    
+    int n;
+    cin >> n;
+
+    float result, sum = 0;
+    for(int i=0; i<n; i++) {
+        int x;
+        cin >> x;
+        sum += x;
+    }
+    result = sum / n;
+    cout << result << endl;
+}
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    solve();
+
+    return 0;
+}
