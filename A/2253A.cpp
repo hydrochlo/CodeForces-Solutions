c6d389e4306a499472728385e52f98724dcfba01
Shahadat Hossain <sht.kcn@gmail.com>
2026-08-08 15:54:26 +0600

    2253A solved

---
+++ A/2253A.cpp
@@ -0,0 +1,43 @@
+// A. The Best Card
+//link: https://codeforces.com/contest/2253/problem/A
+//author: https://codeforces.com/profile/_felu_
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vs = vector<string>;
+using vl = vector<ll>;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define allr(a) a.rbegin(), a.rend()
+#define sz(x) (int)(x).size()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define endl '\n'
+
+
+void solve() {
+    
+    int n;
+    cin >> n;
+    
+    if((n>2) && ((n+1)%2==0 || (n+1)%3==0)) cout << "NO" << endl;
+    else cout << "YES" << endl;
+}
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+       solve();
+    }
+
+    return 0;
+}
