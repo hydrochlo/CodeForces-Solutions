4e1dc564568886bd20131ab458f91f24336baa82
Shahadat Hossain <sht.kcn@gmail.com>
2026-09-07 00:07:59 +0600

    2241A solved

---
+++ A/2241A.cpp
@@ -0,0 +1,43 @@
+// A. Divide and Conquer
+//link: https://codeforces.com/contest/2241/problem/A
+//author: https://codeforces.com/profile/
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vs = vector<string>;
+using vl = vector<ll>;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define allr(a) a.rbegin(), a.rend()
+#define sz(x) (int)(x).size()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define endl '\n'
+
+
+void solve() {
+    
+    int a, b;
+    cin >> a >> b;
+
+    if(a%b==0) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+       solve();
+    }
+
+    return 0;
+}
